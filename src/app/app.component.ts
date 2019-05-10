48b3f09f70f6f815f1936ecfa37d771a91129bc1
FURA\gerard <gcarbo@miraiblau.com>
2019-05-10 12:25:01 +0200

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { FormGroup } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public version: string = environment.VERSION;
+  
   title = 'ngx-password-visible-input';
+  mode = 'mousedown';
+  color = 'primary';
+  raised = true;
+  pwd = 'ThisIsAPassword!';
+
+  // formly data
+  formlyForm = new FormGroup({});
+  formlyFields: FormlyFieldConfig[] = [
+    {
+      key: 'user',
+      type: 'input',
+      templateOptions: {
+        placeholder: 'User',
+        required: true
+      }
+    },
+    {
+      key: 'password',
+      type: 'visiblePassword',
+      templateOptions: {
+        placeholder: 'Password',
+        required: true
+      }
+    }
+  ];
+  formlyModel = {
+    user: '',
+    password: ''
+  };
 }
